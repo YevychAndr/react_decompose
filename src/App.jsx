91ac8833a1ee63b8983fd338b25391d21277a368
Andrii IEvych <yevych27@gmail.com>
2024-11-28 14:59:35 +0200

    fix_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Welcome from './components/Welcome/Welcome';
-import Article from './components/Article/Article';
+import { Header } from './components/Header/Header';
+import { Welcome } from './components/Welcome/Welcome';
+import { Article } from './components/Article/Article';
 // Move each BEM block to a separate component (file) and import them here
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
+export const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
